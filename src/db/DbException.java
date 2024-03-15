1afef432781e67b468144048ac63bebb037a41fc
C1333355 Alex Araujo De Braga <c1333355@interno.bb.com.br>
2024-03-15 17:42:56 -0300

    configuração do banco mysql arquivo properties e operação findById()

---
+++ src/db/DbException.java
@@ -0,0 +1,11 @@
+package db;
+
+public class DbException extends RuntimeException {
+
+	private static final long serialVersionUID = 6552665333347001289L;
+
+	public DbException(String msg) {
+		super(msg);
+	}
+	
+}
